7a014a5ecdf2b8844c3d85b2aefc69e019587794
Hang Li <cecillll.lee@gmail.com>
2020-01-07 10:49:21 +1000

    add basic elements for index and main

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ielab/searchrefiner"
+)
+
+type QueryFormulationPlugin struct {
+}
+
+func (QueryFormulationPlugin) Serve(s searchrefiner.Server, c *gin.Context) {
+	rawQuery := c.PostForm("query")
+	lang := c.PostForm("lang")
+	c.Render(http.StatusOK, searchrefiner.RenderPlugin(searchrefiner.TemplatePlugin("plugin/query-formulation/index.html"), struct {
+		searchrefiner.Query
+		View string
+	}{searchrefiner.Query{QueryString: rawQuery, Language: lang}, c.Query("view")}))
+	return
+}
+
+func (QueryFormulationPlugin) PermissionType() searchrefiner.PluginPermission {
+	return searchrefiner.PluginUser
+}
+
+func (QueryFormulationPlugin) Details() searchrefiner.PluginDetails {
+	return searchrefiner.PluginDetails{
+		Title:       "Query Formulation",
+		Description: "Query formulation tool to formulate query from general input.",
+		Author:      "ielab",
+		Version:     "8.Jan.2020",
+		ProjectURL:  "ielab.io/searchrefiner",
+	}
+}
+
+var Queryformulation = QueryFormulationPlugin{}
